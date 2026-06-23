96ba9e991a42e4653ad6a806bbcbd2e0961b0130
Joseph-Bineesh <joseph.bineesh@ibm.com>
2026-06-23 15:33:23 +0530

    Constants, ErrorCode and messages for ID type diagnostics

--- jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/internal/persistence/ErrorCode.java
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/internal/persistence/ErrorCode.java
@@ -36,7 +36,8 @@ public enum ErrorCode implements IJavaErrorCode {
     InvalidValueInTemporalAnnotation,
     DuplicateVersionAnnotationInClass,
     DuplicateVersionAnnotationInHierarchy,
-    InvalidVersionFieldOrPropertyType;
+    InvalidVersionFieldOrPropertyType,
+    InvalidIdType;
 
     /**
      * {@inheritDoc}
